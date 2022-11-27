a57157042d70e7ae0eb7c278c60200dfe05519ff
Ben-Hur <lukbenhur@gmail.com>
2022-11-27 18:44:12 -0300

    First Class: Position

---
+++ Project-Xadrez/src/aplicacao/Program.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import tabuleiro.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position position = new Position(5, 6);
+		
+		System.out.println(position);
+	}
+}
---
+++ Project-Xadrez/src/tabuleiro/Position.java
@@ -0,0 +1,44 @@
+package tabuleiro;
+
+public class Position {
+
+	private Integer row;
+	private Integer column;
+	
+	
+	public Position() {
+	}
+
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	
+	//metodo de setar novas csasas para as peças 
+	public void setValues(Integer row , Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return getRow() + ", " + getColumn();
+	}
+	
+}
